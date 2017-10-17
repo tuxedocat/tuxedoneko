f1acac96535dce3be7e0585fd0ecb3cfa6bdac8e
tuxedocat <tuxedokatzen@gmail.com>
2017-10-17 22:53:10 +0900

    sass -> scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task("default", function () {
-  gulp.src('sass/style.sass')
+  gulp.src('sass/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer())
     .pipe(gulp.dest('static/css/'));
 });
 
 gulp.task("watch", function () {
-  gulp.watch('sass/style.sass', function() {
-    gulp.src('sass/style.sass')
+  gulp.watch('sass/style.scss', function() {
+    gulp.src('sass/style.scss')
       .pipe(sass({outputStyle: 'compressed'}))
       .pipe(autoprefixer())
       .pipe(gulp.dest('static/css/'));
